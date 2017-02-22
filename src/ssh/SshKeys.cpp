25be679b18d1d20d1cbbc538c344434f842610ca
Ryan C. Scott <ryan@5pmCasual.com>
2017-02-21 16:43:06 -0800

    All keys are now attempted to be added .Matching on hostname needs a little more thought as that can change based on DHCP and the like.

--- src/ssh/SshKeys.cpp
+++ src/ssh/SshKeys.cpp
@@ -49,7 +49,7 @@ void SshKeys::addKey(Entry *entry, QString hostname, QProcessEnvironment &env)
 {
     auto url = QUrl(entry->url());
     
-    if (url.scheme() == "ssh" && QString::compare(hostname, url.host(), Qt::CaseInsensitive) == 0) {
+    if (url.scheme() == "ssh") { // && QString::compare(hostname, url.host(), Qt::CaseInsensitive) == 0) {
         qDebug() << "SSH: Adding key:" << url.path();
 
         QProcess add;
